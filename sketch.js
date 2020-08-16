20afa2f1f676b25e71a0a00c395785038f208890
yashkothari1234 <68227727+yashkothari1234@users.noreply.github.com>
2020-08-16 16:20:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+
+function preload()
+{
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+
+
+Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(255,255,255); 
+  
+  
+
+
+
+
+
+
+
+  drawSprites();
+}
